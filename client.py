626da754bd35ddc16c9ba3015928abbf8fbc9e7b
ray <rayylim@gmail.com>
2015-04-08 00:35:47 -0700

    Fix disconnecting client.

--- client.py
+++ client.py
@@ -1,60 +1,22 @@
 __author__ = 'ray'
 
-# import socket
-# import sys
-#
-# # Create a UDS socket
-# sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-#
-# # Connect the socket to the port where the server is listening
-# server_address = './uds_socket'
-# print >>sys.stderr, 'connecting to %s' % server_address
-# try:
-#     sock.connect(server_address)
-# except socket.error, msg:
-#     print >>sys.stderr, msg
-#     sys.exit(1)
-#
-# try:
-#
-#     # Send data
-#     message = 'This is the message.  It will be repeated.'
-#     print >>sys.stderr, 'sending "%s"' % message
-#     sock.sendall(message)
-#
-#     amount_received = 0
-#     amount_expected = len(message)
-#
-#     while amount_received < amount_expected:
-#         data = sock.recv(16)
-#         amount_received += len(data)
-#         print >>sys.stderr, 'received "%s"' % data
-#
-# finally:
-#     print >>sys.stderr, 'closing socket'
-#     sock.close()
-#
+import socket
+import select
+import sys
 
-# telnet program example
-import socket, select, string, sys
 
-def prompt() :
+def prompt():
     sys.stdout.write('<You> ')
     sys.stdout.flush()
 
-#main function
+
 if __name__ == "__main__":
 
     s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     s.settimeout(2)
 
-    # connect to remote host
-    try :
-        server_address = './uds_socket'
-        s.connect(server_address)
-    except:
-        print 'Unable to connect'
-        sys.exit()
+    server_address = './uds_socket'
+    s.connect(server_address)
 
     print 'Connected to remote host. Start sending messages'
     prompt()
@@ -63,22 +25,21 @@ if __name__ == "__main__":
         socket_list = [sys.stdin, s]
 
         # Get the list sockets which are readable
-        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+        read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
 
         for sock in read_sockets:
-            #incoming message from remote server
+            # incoming message from remote server
             if sock == s:
                 data = sock.recv(4096)
-                if not data :
+                if not data:
                     print '\nDisconnected from chat server'
                     sys.exit()
-                else :
-                    #print data
+                else:
                     sys.stdout.write(data)
                     prompt()
 
-            #user entered a message
-            else :
+            # user entered a message
+            else:
                 msg = sys.stdin.readline()
                 s.send(msg)
                 prompt()
--- server.py
+++ server.py
@@ -1,112 +1,77 @@
-__author__ = 'ray'
+SERVER_ADDRESS = './uds_socket'
+RECV_BUFFER = 4096  # Advisable to keep it as an exponent of 2
 
-# import socket
-# import sys
-# import os
-#
-# server_address = './uds_socket'
-#
-# # Make sure the socket does not already exist
-# try:
-#     os.unlink(server_address)
-# except OSError:
-#     if os.path.exists(server_address):
-#         raise
-#
-# # Create a UDS socket
-# sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-#
-#
-# # Bind the socket to the port
-# print >>sys.stderr, 'starting up on %s' % server_address
-# sock.bind(server_address)
-#
-# # Listen for incoming connections
-# sock.listen(1)
-#
-# while True:
-#     # Wait for a connection
-#     print >>sys.stderr, 'waiting for a connection'
-#     connection, client_address = sock.accept()
-#     try:
-#         print >>sys.stderr, 'connection from', client_address
-#
-#         # Receive the data in small chunks and retransmit it
-#         while True:
-#             data = connection.recv(16)
-#             print >>sys.stderr, 'received "%s"' % data
-#             if data:
-#                 print >>sys.stderr, 'sending data back to the client'
-#                 connection.sendall(data)
-#             else:
-#                 print >>sys.stderr, 'no more data from', client_address
-#                 break
-#
-#     finally:
-#         # Clean up the connection
-#         connection.close()
+import os
+import socket
+import select
 
-# Tcp Chat server
+# Function to broadcast chat messages to all connected clients
+def broadcast_data(connection_list, socket_server, message):
+    # Do not send the message to master socket and the client who has send us the message
+    for connection in connection_list:
+        if connection != server_socket:
+            try:
+                connection.send(message)
+            except socket.error:
+                # client connection closed
+                connection.close()
+                if connection in connection_list:
+                    connection_list.remove(sock)
 
-import socket, select
 
-#Function to broadcast chat messages to all connected clients
-def broadcast_data (sock, message):
-    #Do not send the message to master socket and the client who has send us the message
-    for socket in CONNECTION_LIST:
-        if socket != server_socket and socket != sock :
-            try :
-                socket.send(message)
-            except :
-                # broken socket connection may be, chat client pressed ctrl+c for example
-                socket.close()
-                CONNECTION_LIST.remove(socket)
+def create_socket_server():
+    sock_server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        os.unlink(SERVER_ADDRESS)
+    except OSError:
+        if os.path.exists(SERVER_ADDRESS):
+            raise
+    sock_server.bind(SERVER_ADDRESS)
+    sock_server.listen(10)
+    return sock_server
+
 
 if __name__ == "__main__":
 
     # List to keep track of socket descriptors
-    CONNECTION_LIST = []
-    RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
+    connection_list = []
 
-    server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    # this has no effect, why ?
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    server_address = './uds_socket'
-    server_socket.bind(server_address)
-    server_socket.listen(10)
+    server_socket = create_socket_server()
 
     # Add server socket to the list of readable connections
-    CONNECTION_LIST.append(server_socket)
+    connection_list.append(server_socket)
 
     while 1:
         # Get the list sockets which are ready to be read through select
-        read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
+        read_sockets, write_sockets, error_sockets = select.select(connection_list, [], [])
 
         for sock in read_sockets:
-            #New connection
+            # New connection
             if sock == server_socket:
                 print "new connection"
                 # Handle the case in which there is a new connection received through server_socket
                 sockfd, addr = server_socket.accept()
                 print "new connection", sockfd, addr
-                CONNECTION_LIST.append(sockfd)
+                connection_list.append(sockfd)
 
-                broadcast_data(sockfd, "[%s:%s] entered room\n" % (sockfd, addr))
+                broadcast_data(connection_list, server_socket, "[%s:%s] entered room\n" % (sockfd, addr))
 
-            #Some incoming message from a client
+            # Some incoming message from a client
             else:
-                # Data recieved from client, process it
-                try:
-                    data = sock.recv(RECV_BUFFER)
-                    if data:
-                        broadcast_data(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)
+                # Data received from client, process it
+                data = sock.recv(RECV_BUFFER)
+                if data:
+                    broadcast_data(connection_list, server_socket,
+                                   "\r" + '<' + str(sock.getpeername()) + '> ' + data)
+                else:
+                    print "Client (%s, %s) is offline" % (sock, addr)
+                    broadcast_data(connection_list, server_socket, "Client (%s, %s) is offline" % (sock, addr))
 
-                except:
-                    broadcast_data(sock, "Client (%s, %s) is offline" % (sockfd, addr))
-                    print "Client (%s, %s) is offline" % (sockfd, addr)
                     sock.close()
-                    CONNECTION_LIST.remove(sock)
+                    if sock in connection_list:
+                        connection_list.remove(sock)
                     continue
 
     server_socket.close()
+
